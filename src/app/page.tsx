bd22678fde4a9dc6382e7cff6e8ccde0a41ccaab
ndelanhese <nnathanh3@gmail.com>
2024-02-16 22:23:10 -0300

    feat(home): add initial layou and added shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-	return <h1>Hello World!!</h1>;
+	return (
+		<main className="w-full bg-primary h-screen grid sm:grid-cols-2 grid-cols-1 grid-rows-2 sm:grid-rows-1 sm:py-4 px-4 sm:px-0">
+			<div className="flex flex-1 items-center justify-center border-b sm:border-b-0 sm:border-r">
+				<h1 className="text-primary-foreground">from</h1>
+			</div>
+			<div className="flex flex-1 items-center justify-center">
+				<h1 className="text-primary-foreground">to</h1>
+			</div>
+		</main>
+	);
 };
 
 export default Home;
